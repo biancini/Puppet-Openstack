37e20471d76b98fd38685efa418850d139b29ddc
Andrea Biancini <andrea.biancini@gmail.com>
2013-03-26 12:41:43 +0000

    Correct garrstack module

--- garr/garrstack/manifests/configs.pp
+++
@@ -1,7 +0,0 @@
-class garrstack::configs {
-  
-    nova_config { 'live_migration_flag':
-      value => 'VIR_MIGRATE_UNDEFINE_SOURCE,VIR_MIGRATE_PEER2PEER,VIR_MIGRATE_LIVE'
-    }
-
-}
--- garr/garrstack/manifests/init.pp
+++ garr/garrstack/manifests/init.pp
@@ -1,3 +1,4 @@
+import "classes/*.pp"
 import "definitions/*.pp"
 
 class garrstack { }
