b0c6d1d0b63c5641c8bc02a1ff4a1261ec69e34a
Andrea Biancini <andrea.biancini@gmail.com>
2013-04-11 11:36:36 +0200

    Groups

--- garr/garrstack/manifests/classes/usergroups.pp
+++ garr/garrstack/manifests/classes/usergroups.pp
@@ -3,43 +3,43 @@ class garrstack::usergroups {
     'libvirt-qemu-usr':
       path => '/etc/passwd',
       line => "libvirt-qemu:x:111:117:Libvirt Qemu,,,:/var/lib/libvirt:/bin/false",
-      match => 'libvirt-qemu';
+      match => '^libvirt-qemu:';
     
     'libvirt-dnsmasq-usr':
       path => '/etc/passwd',
       line => "libvirt-dnsmasq:x:112:118:Libvirt Dnsmasq,,,:/var/lib/libvirt/dnsmasq:/bin/false",
-      match => 'libvirt-dnsmasq';
+      match => '^libvirt-dnsmasq:';
       
     'nova-usr':
       path => '/etc/passwd',
       line => "nova:x:113:119::/var/lib/nova:/bin/false",
-      match => 'nova';
+      match => '^nova:';
 
     'cinder-usr':
       path => '/etc/passwd',
       line => "cinder:x:114:120::/var/lib/cinder:/bin/sh",
-      match => 'cinder'; 
+      match => '^cinder:'; 
   }
   
   file_line {
     'kvm-grp':
       path => '/etc/group',
       line => "kvm:x:117:",
-      match => 'kvm';
+      match => '^kvm:';
     
     'libvirtd-grp':
       path => '/etc/group',
       line => "libvirtd:x:118:nova",
-      match => 'libvirtd';
+      match => '^libvirtd:';
 	
 	  'nova-grp':
       path => '/etc/group',
       line => "nova:x:119:cinder",
-      match => 'nova';
+      match => '^nova:';
   
     'cinder-grp':
       path => '/etc/group',
       line => "cinder:x:120:nova",
-      match => 'cinder';
+      match => '^cinder:';
   }
 }
